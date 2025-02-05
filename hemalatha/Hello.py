d336cd9c39903269cc31cab7e3b195163eb7fc2c
Hemalatha M <hemalatham236@gmail.com>
2025-02-05 18:45:07 +0530

    till stock price save

---
+++ hemalatha/Hello.py
@@ -0,0 +1,85 @@
+import streamlit as st
+
+st.title("This is a title")
+st.title("Hemalatha is Best")
+
+
+
+import streamlit as st
+
+st.header("_Streamlit_ is :blue[cool] :sunglasses:")
+st.header("This is a header with a divider", divider="gray")
+st.header("These headers have rotating dividers", divider=True)
+st.header("One", divider=True)
+st.header("Two", divider=True)
+st.header("Three", divider=True)
+st.header("Four", divider=True)
+
+import streamlit as st
+
+st.subheader("_Streamlit_ is :blue[cool] :sunglasses:")
+st.subheader("This is a subheader with a divider", divider="gray")
+st.subheader("These subheaders have rotating dividers", divider=True)
+st.subheader("One", divider=True)
+st.subheader("Two", divider=True)
+st.subheader("Three", divider=True)
+st.subheader("Four", divider=True)
+
+
+import streamlit as st
+
+agree = st.checkbox("I agree")
+
+if agree:
+    st.write("Great!")
+
+import streamlit as st
+
+genre = st.radio(
+    "What's your favorite movie genre",
+    [":rainbow[Comedy]", "***Drama***", "Documentary :movie_camera:"],
+    captions=[
+        "Laugh out loud.",
+        "Get the popcorn.",
+        "Never stop learning.",
+    ],
+)
+
+if genre == ":rainbow[Comedy]":
+    st.write("You selected comedy.")
+else:
+    st.write("You didn't select comedy.")
+
+import streamlit as st
+
+# Store the initial value of widgets in session state
+if "visibility" not in st.session_state:
+    st.session_state.visibility = "visible"
+    st.session_state.disabled = False
+    st.session_state.horizontal = False
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.checkbox("Disable radio widget", key="disabled")
+    st.checkbox("Orient radio options horizontally", key="horizontal")
+
+with col2:
+    st.radio(
+        "Set label visibility 👇",
+        ["visible", "hidden", "collapsed"],
+        key="visibility",
+        label_visibility=st.session_state.visibility,
+        disabled=st.session_state.disabled,
+        horizontal=st.session_state.horizontal,
+    )
+import streamlit as st
+
+st.button("Reset", type="primary")
+if st.button("Say hello"):
+    st.write("Why hello there")
+else:
+    st.write("Goodbye")
+
+if st.button("Aloha", type="tertiary"):
+    st.write("Ciao")
